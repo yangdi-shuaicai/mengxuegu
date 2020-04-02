ffdc1084e884f003888379b962efe3c929a7194e
yangdi <984058652@qq.com>
2020-04-02 17:30:30 +0800

    Initial commit

---
+++ src/main/java/com/yangdi/mengxuegu/MengxueguApplication.java
@@ -0,0 +1,13 @@
+package com.yangdi.mengxuegu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MengxueguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MengxueguApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangdi/mengxuegu/MengxueguApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yangdi.mengxuegu;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MengxueguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
